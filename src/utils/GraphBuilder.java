b5dfebaf6b4d912eb02a858aff8177073f47b910
vandegor <Adamos500@gmail.com>
2016-06-22 16:58:44 +0200

    change project directory

--- src/utils/GraphBuilder.java
+++ src/utils/GraphBuilder.java
@@ -1,3 +1,4 @@
+package utils;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
--- src/utils/Node.java
+++ src/utils/Node.java
@@ -1,3 +1,4 @@
+package utils;
 import java.util.Map;
 import java.util.TreeMap;
 
